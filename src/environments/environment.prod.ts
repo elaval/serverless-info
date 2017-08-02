80182ea727568d44ad6ae2bcb65f1916cc4c5e03
Ernesto Laval <ernesto.laval@gmail.com>
2017-08-02 05:21:03 +0100

    Add firebase keys to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDrksp-W9-I6Z2jKoAdsQkPuaFZGmidi68',
+    authDomain: 'datavis-8d136.firebaseapp.com',
+    databaseURL: 'https://datavis-8d136.firebaseio.com/',
+    projectId: 'datavis-8d136',
+    storageBucket: 'gs://datavis-8d136.appspot.com',
+    messagingSenderId: '<your-messaging-sender-id>'
+  }
 };
